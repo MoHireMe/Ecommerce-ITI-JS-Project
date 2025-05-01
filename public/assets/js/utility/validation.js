8a180cad29270d06b7cf228c01d282820bc5cd21
MoHireMe <ashrof.learn@gmail.com>
2025-05-01 14:31:02 +0300

    register Page UI and Logic Done

--- public/assets/js/utility/validation.js
+++ public/assets/js/utility/validation.js
@@ -7,3 +7,13 @@ export function validatePassword(pass) {
   const regex = /^(?=.*[a-z])(?=.*\d).{8,}$/;
   return regex.test(pass);
 }
+
+export function validateName(name) {
+  const re = /^[a-zA-Z\s]{3,}$/;
+  return re.test(name.trim());
+}
+
+export function validatePhone(phone) {
+  const re = /^01[0125][0-9]{8}$/;
+  return re.test(phone.trim());
+}

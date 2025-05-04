63a6895f702f7676b76e4c26fe95972be71ecc6a
MoHireMe <ashrof.learn@gmail.com>
2025-05-05 01:38:13 +0300

    admin product Manage functionallity Done

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -15,6 +15,14 @@ export async function getApprovedProduct(id) {
   return data[0];
 }
 
+export async function getAllProducts() {
+  const res = await fetch("/products");
+  const data = await res.json();
+  if (!res.ok || !data || data.length == 0) throw Error("Products Not found");
+  if (!res) throw Error("Unexpected Error");
+  return data;
+}
+
 export async function getSellerNameById(id) {
   if (!id) throw Error("Invalid Users ID");
   const res = await fetch(`/users?role=seller&id=${id}`);
@@ -40,4 +48,20 @@ export async function getAllReviewsByProductId(id) {
   if (!res.ok) throw Error("Reviews Not found");
   if (!res) throw Error("Unexpected Error");
   return data;
+}
+
+export async function deleteProductById(id) {
+  if (!id) throw Error("Invalid Product ID");
+  await fetch(`/products/${id}`, {
+    method: "DELETE",
+  });
+}
+
+export async function updateProductById(updatedItem) {
+  const res = await fetch(`http://localhost:3000/products/${updatedItem.id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(updatedItem),
+  });
+  if (!res.ok) throw new Error("Update failed");
 }
---
+++ public/assets/js/dashboard/admin-product-manage.js
@@ -0,0 +1,145 @@
+import {
+  getAllProducts,
+  deleteProductById,
+  updateProductById,
+} from "../api.js";
+
+const theadTag = document.querySelector("thead");
+const tbodyTag = document.querySelector("thead");
+const dialog = document.querySelector("#edit-dialog");
+const form = document.querySelector("#edit-form");
+const cancelBtn = document.querySelector("#cancel-btn");
+
+const displayTableHeads = (prod) => {
+  const keys = Object.keys(prod[0]);
+  const thDel = document.createElement("th");
+  const thEdit = document.createElement("th");
+  const thApproval = document.createElement("th");
+
+  keys.forEach((key) => {
+    const th = document.createElement("th");
+    th.innerText = key;
+    theadTag.append(th);
+  });
+  thDel.innerText = "Delete";
+  thEdit.innerText = "Edit";
+  thApproval.innerText = "Approval Toggle";
+
+  theadTag.append(thDel);
+  theadTag.append(thEdit);
+  theadTag.append(thApproval);
+};
+
+const displayTableBody = (prod) => {
+  prod.forEach((item) => {
+    const tr = document.createElement("tr");
+    tr.setAttribute("data-id", item.id);
+
+    for (const data in item) {
+      const td = document.createElement("td");
+      if (item == "approved") {
+        const btn = document.createElement("button");
+        btn.classList.add("toggle-approve");
+      }
+      td.innerText = item[data];
+      tr.append(td);
+    }
+
+    for (const el of [
+      ["del-btn", "🗑️"],
+      ["edit-btn", "Edit"],
+    ]) {
+      const td = document.createElement("td");
+      const btn = document.createElement("button");
+
+      btn.classList.add(el[0]);
+      btn.innerText = el[1];
+
+      td.append(btn);
+      tr.append(td);
+    }
+    const toggleApprovalTd = document.createElement("td");
+    const toggleApprovalBtn = document.createElement("button");
+
+    toggleApprovalBtn.classList.add("toggle-approval");
+    toggleApprovalBtn.classList.add(item.approved ? "reject" : "approve");
+    toggleApprovalBtn.innerText = item.approved ? "reject" : "approve";
+
+    toggleApprovalTd.append(toggleApprovalBtn);
+    tr.append(toggleApprovalTd);
+
+    tbodyTag.append(tr);
+  });
+};
+
+try {
+  const product = await getAllProducts();
+  displayTableHeads(product);
+  displayTableBody(product);
+} catch (error) {
+  alert(error.message);
+}
+
+const rows = tbodyTag.querySelectorAll("tr");
+
+rows.forEach((row) => {
+  row.addEventListener("click", async (e) => {
+    e.stopPropagation();
+
+    if (e.target.classList.contains("del-btn")) {
+      await deleteProductById(row.getAttribute("data-id"));
+      row.remove();
+      alert("Item Deleted");
+    }
+
+    if (e.target.classList.contains("edit-btn")) {
+      const data = row.querySelectorAll("td");
+      document.querySelector("#edit-id").value = data[0].innerText;
+      document.querySelector("#edit-name").value = data[2].innerText;
+      document.querySelector("#edit-description").value = data[3].innerText;
+      document.querySelector("#edit-cat").value = data[4].innerText;
+      document.querySelector("#edit-price").value = data[5].innerText;
+      document.querySelector("#edit-stock").value = data[6].innerText;
+      document.querySelector("#edit-Image").value = data[8].innerText;
+
+      dialog.showModal();
+    }
+
+    if (e.target.classList.contains("toggle-approval")) {
+      const currentApproval = Boolean(row.querySelectorAll("td")[7].innerText);
+      const obj = {
+        id: row.getAttribute("data-id"),
+        approved: !currentApproval,
+      };
+
+      await updateProductById(obj);
+      location.reload();
+    }
+  });
+});
+
+cancelBtn.addEventListener("click", () => {
+  dialog.close();
+});
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const updatedItem = {
+    id: document.querySelector("#edit-id").value,
+    name: document.querySelector("#edit-name").value,
+    description: document.querySelector("#edit-description").value,
+    category: document.querySelector("#edit-cat").value,
+    price: document.querySelector("#edit-price").value,
+    stock: document.querySelector("#edit-stock").value,
+    image: document.querySelector("#edit-Image").value,
+  };
+
+  try {
+    await updateProductById(updatedItem);
+    dialog.close();
+    location.reload();
+  } catch (err) {
+    alert(err.message);
+  }
+});
